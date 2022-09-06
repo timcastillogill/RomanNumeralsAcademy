9e1acd2e3af0fc0ced9e0ec1f1c3fd75936cfdf3
timcastillogill <timgillviola@gmail.com>
2022-09-06 09:34:18 +0100

    REFACTOR: Repeat method upto number 3

--- src/main/java/RomanNumeralConverter.java
+++ src/main/java/RomanNumeralConverter.java
@@ -1,11 +1,5 @@
 public class RomanNumeralConverter {
 	public String convert(int arabic) {
-		if (arabic == 2) {
-			return "II";
-		}
-		if (arabic == 3) {
-			return "III";
-		}
-		return "I";
+		return "I".repeat(arabic);
 	}
 }
