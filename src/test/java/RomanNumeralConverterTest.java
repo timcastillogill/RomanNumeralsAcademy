c35183af280f153278679c19f6e4131fe3adacc8
timcastillogill <timgillviola@gmail.com>
2022-09-06 09:32:24 +0100

    GREEN: Passing with numbers upto 3

--- src/test/java/RomanNumeralConverterTest.java
+++ src/test/java/RomanNumeralConverterTest.java
@@ -10,4 +10,15 @@ class RomanNumeralConverterTest {
 	given_one_return_I() {
 		assertThat(new RomanNumeralConverter().convert(1), is("I"));
 	}
+	@Test
+	public void
+	given_two_return_II() {
+		assertThat(new RomanNumeralConverter().convert(2), is("II"));
+	}
+
+	@Test
+	public void
+	given_three_return_III() {
+		assertThat(new RomanNumeralConverter().convert(3), is("III"));
+	}
 }
